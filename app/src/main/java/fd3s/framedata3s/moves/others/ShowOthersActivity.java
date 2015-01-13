e2e09cb727f62f2a47c1885f2d670949e8c1a61a
Kivikavi <n0303731@ntu.ac.uk>
2015-01-13 23:58:29 +0000

    prettified other data screen

--- app/src/main/java/fd3s/framedata3s/moves/others/ShowOthersActivity.java
+++ app/src/main/java/fd3s/framedata3s/moves/others/ShowOthersActivity.java
@@ -1,62 +1,81 @@
 package fd3s.framedata3s.moves.others;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ImageView;
-import android.widget.ListView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Map;
 
 import fd3s.framedata3s.R;
-import fd3s.framedata3s.adapters.AlternatingColorListViewAdapter;
 import fd3s.framedata3s.sdo.CharSDO;
 import fd3s.framedata3s.utils.FrameDataProvider;
 import fd3s.framedata3s.utils.ResourceHelper;
 
 public class ShowOthersActivity extends ActionBarActivity {
+
+
+    private static final String JUMP_PATTERN = "jump";
+    private static final String DASH_PATTERN = "Dash";
+    private static final String WAKEUP_PATTERN = "Wakeup";
+
     private ShowOthersActivity ref = this;
+    private LayoutInflater vi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_character);
+        setContentView(R.layout.other_detail_layout);
+        vi = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
         int characterId = getIntent().getIntExtra(ResourceHelper.ResourceIds.CHARACTER_ID.name(), 0);
         this.setTitle(ResourceHelper.CharacterNames[characterId] + " Frame Data");
         CharSDO charSDO = FrameDataProvider.getInstance(characterId, this).getCharSDO();
 
-        TextView tv = (TextView)findViewById(R.id.page_heading);
-        ImageView ivCharImage = (ImageView)findViewById(R.id.char_image);
+        TextView tv = (TextView) findViewById(R.id.page_heading);
+        ImageView ivCharImage = (ImageView) findViewById(R.id.char_image);
 
         tv.setText("Other");
         ivCharImage.setImageResource(ResourceHelper.ThumbIds[characterId]);
 
-        if (charSDO != null) {
-
-            final ListView lvMoves = (ListView) findViewById(R.id.move_names);
-            ArrayList<String> aMoveNames = new ArrayList<String>();
-
-            Iterator entries = charSDO.misc.entrySet().iterator();
-            while (entries.hasNext()) {
-                Map.Entry<String, String> thisEntry = (Map.Entry<String, String>) entries.next();
-                String key = thisEntry.getKey();
-                String value = thisEntry.getValue();
-                aMoveNames.add(key + " : " + value);
-            }
-
-            AlternatingColorListViewAdapter adapter = new AlternatingColorListViewAdapter(this,
-                    R.layout.move_layout, aMoveNames);
-
-            lvMoves.setAdapter(adapter);
+        if (charSDO.misc != null) {
+            MapKeyValuePairsToLinearLayout(charSDO.misc, (LinearLayout) findViewById(R.id.jump_list_view), JUMP_PATTERN);
+            MapKeyValuePairsToLinearLayout(charSDO.misc, (LinearLayout) findViewById(R.id.dash_list_view), DASH_PATTERN);
+            MapKeyValuePairsToLinearLayout(charSDO.misc, (LinearLayout) findViewById(R.id.wake_up_list_view), WAKEUP_PATTERN);
         }
+    }
+
+    private void MapKeyValuePairsToLinearLayout(Map<String, String> map, LinearLayout root, String pattern) {
+        Iterator entries = map.entrySet().iterator();
+
+        while (entries.hasNext()) {
+            Map.Entry<String, String> thisEntry = (Map.Entry<String, String>) entries.next();
+            String key = thisEntry.getKey();
+
+            if (key.contains(pattern)) { //easier than formatting all the data, I'm lazy
+                String value = thisEntry.getValue();
+                addViewToLinearLayout(root, key, value);
+            }
+        }
+    }
+
+    private void addViewToLinearLayout(LinearLayout root, String key, String value) {
+        View row = vi.inflate(R.layout.key_value_layout, null);
+
+        TextView tv_key = (TextView) row.findViewById(R.id.key);
+        tv_key.setText(key);
+
+        TextView tv_value = (TextView) row.findViewById(R.id.value);
+        tv_value.setText(value);
+
+        root.addView(row);
     }
 
     @Override
